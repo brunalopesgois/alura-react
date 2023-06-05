fd8c11aaadcbb77e7619b00e5b6380317a831183
Bruna Gois <bruna.gois@bitcapital.com.br>
2023-06-05 11:29:56 -0300

    feat (wip): use states

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './List.module.scss';
 import Item from './Item';
 
 function List() {
-  const tasks = [
+  const [tasks, setTasks] = useState([
     {
       task: 'React',
       time: '02:00:00',
@@ -16,11 +16,17 @@ function List() {
       task: 'Typescript',
       time: '03:00:00',
     },
-  ];
+  ]);
 
   return (
     <aside className={style.taskList}>
-      <h2>Estudos do dia</h2>
+      <h2
+        onClick={() => {
+          setTasks([...tasks, { task: 'Estudar estado', time: '05:00:00' }]);
+        }}
+      >
+        Estudos do dia
+      </h2>
       <ul>
         {tasks.map((item, index) => (
           <Item key={index} {...item} />
