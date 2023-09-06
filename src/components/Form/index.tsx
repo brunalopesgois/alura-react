14134ce5698f92e3a23bc912a82b637872fb75c0
Bruna Lopes <bruna-chan@live.com>
2023-09-06 11:52:22 -0300

    feat: finish use states

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,17 +1,43 @@
 import React from 'react';
 import Button from '../Button';
 import style from './Form.module.scss';
+import { ITask } from '../../types/task';
 
-class Form extends React.Component {
+class Form extends React.Component<{
+  setTasks: React.Dispatch<React.SetStateAction<ITask[]>>;
+}> {
+  state = {
+    task: '',
+    time: '00:00',
+  };
+
+  addTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    this.props.setTasks((oldTasks) => [
+      ...oldTasks,
+      {
+        ...this.state,
+      },
+    ]);
+    this.setState({
+      task: '',
+      time: '00:00',
+    });
+  }
+
   render() {
     return (
-      <form className={style.newTask}>
+      <form className={style.newTask} onSubmit={this.addTask.bind(this)}>
         <div className={style.inputContainer}>
           <label htmlFor='tarefa'>Adicione um novo estudo</label>
           <input
             type='text'
             name='tarefa'
             id='tarefa'
+            value={this.state.task}
+            onChange={(event) =>
+              this.setState({ ...this.state, task: event.target.value })
+            }
             placeholder='O que você quer estudar'
             required
           />
@@ -23,6 +49,10 @@ class Form extends React.Component {
             step='1'
             name='tempo'
             id='tempo'
+            value={this.state.time}
+            onChange={(event) =>
+              this.setState({ ...this.state, time: event.target.value })
+            }
             min='00:00:00'
             max='01:30:00'
             required
