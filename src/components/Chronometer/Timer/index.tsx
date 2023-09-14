1cbc25b50371fa0d014cd84e1373e3dae8aa153a
Bruna Lopes <bruna-chan@live.com>
2023-09-14 10:24:08 -0300

    feat: show time in timer

--- src/components/Chronometer/Timer/index.tsx
+++ src/components/Chronometer/Timer/index.tsx
@@ -1,13 +1,21 @@
 import style from './Timer.module.scss';
 
-export default function Timer() {
+interface IProps {
+  time: number | undefined;
+}
+
+export default function Timer({ time = 0 }: IProps) {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  const [minuteTen, minuteUnit] = String(minutes).padStart(2, '0');
+  const [secondTen, secondUnit] = String(seconds).padStart(2, '0');
   return (
     <>
-      <span className={style.timerNumber}>0</span>
-      <span className={style.timerNumber}>0</span>
+      <span className={style.timerNumber}>{minuteTen}</span>
+      <span className={style.timerNumber}>{minuteUnit}</span>
       <span className={style.timerDiv}>:</span>
-      <span className={style.timerNumber}>0</span>
-      <span className={style.timerNumber}>0</span>
+      <span className={style.timerNumber}>{secondTen}</span>
+      <span className={style.timerNumber}>{secondUnit}</span>
     </>
   );
 }
--- src/components/Chronometer/index.tsx
+++ src/components/Chronometer/index.tsx
@@ -1,13 +1,28 @@
 import Button from '../Button';
 import Timer from './Timer';
 import style from './Chronometer.module.scss';
+import { ITask } from '../../types/task';
+import { useEffect, useState } from 'react';
+import { timeToSeconds } from '../../common/utils/time';
 
-export default function Chronometer() {
+interface IProps {
+  selected: ITask | undefined;
+}
+
+export default function Chronometer({ selected }: IProps) {
+  const [time, setTime] = useState<number>();
+
+  useEffect(() => {
+    if (selected?.time) {
+      setTime(timeToSeconds(selected.time));
+    }
+  }, [selected]);
+
   return (
     <div className={style.chronometer}>
       <p className={style.title}>Escolha um card e inicie o cronômetro</p>
       <div className={style.timerWrapper}>
-        <Timer />
+        <Timer time={time} />
       </div>
       <Button>Começar!</Button>
     </div>
